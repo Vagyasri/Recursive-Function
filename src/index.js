d951943aa577dffe41f7a09726277b011fa4cdac
Vagyasri <vagyasri2011@gmail.com>
2021-11-12 13:34:01 +0100

    Remove error

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
+/* eslint-disable max-len */
 import './style.css';
 
 // Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
 
 // without recursive function
 function sum(arr, n) {
-    let sum = 0;
-    for(let i = 0; i < n; i++) {
-      sum += arr[i];
-    }
-    return sum;
+  let sum = 0;
+  for (let i = 0; i < n; i += 1) {
+    sum += arr[i];
   }
+  return sum;
+}
 
 // with recursive function
-function sum(arr, n) {
-    if(n <= 0) {
-      return 0;
-    } else {
-      return arr[n-1] + sum(arr, n-1);
-    }
+function sumRec(arr, n) {
+  if (n <= 0) {
+    return 0;
   }
+  return arr[n - 1] + sum(arr, n - 1);
+}
 
 // Test Cases
-console.log(sum([1], 0)) // 0
+console.log(sum([1], 0)); // 0
 
-console.log(sum([2, 3, 4], 1)) // 2
+console.log(sumRec([2, 3, 4], 1)); // 2
 
-console.log(sum([2, 3, 4, 5], 3)) // 9
+console.log(sum([2, 3, 4, 5], 3)); // 9
