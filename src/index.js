d478cbc49dfc1673d810648d3c254970a8a532a5
Vagyasri <vagyasri2011@gmail.com>
2021-11-12 11:46:39 +0100

    Add without recursion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './style.css';
 
+// without recursive function
+function sum(arr, n) {
+    let sum = 0;
+    for(let i = 0; i < n; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+
 // with recursive function
 function sum(arr, n) {
     if(n <= 0) {
